560490a12845db797d0ddf22bbd01ea2d50337ae
Thalia Nero <chrisvn00@gmail.com>
2021-12-28 21:25:44 -0600

    Why use unsafe impls when you could not?

--- src/traits.rs
+++ src/traits.rs
@@ -1,13 +1,11 @@
+use core::cell::Cell;
 use core::marker::PhantomData;
 
 /// Marker for allocators that are neither Send nor Sync.
-pub struct LocalTrait(PhantomData<*const ()>);
+pub struct LocalTrait(PhantomData<&'static Cell<()>>);
 
 /// Marker for allocators that are Send but not Sync.
-pub struct SendTrait(PhantomData<*const ()>);
-
-/// SAFETY: This should be Send but not Sync for marking allocators.
-unsafe impl Send for SendTrait {}
+pub struct SendTrait(PhantomData<Cell<()>>);
 
 /// Marker for allocators that are both Send and Sync.
 pub struct SendSyncTrait(PhantomData<()>);
